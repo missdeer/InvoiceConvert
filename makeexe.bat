5a83b827a0edeb2e7df44036aa16c20d1b8dbb0f
Fan Yang <missdeer@gmail.com>
2025-11-14 13:20:15 +0800

    rename exe file

--- makeexe.bat
+++ makeexe.bat
@@ -1,2 +1,2 @@
 d:\Python313\Scripts\pyinstaller.exe --onefile --clean main.py --icon=%~dp0\invoice.ico --exclude-module=tkinter --exclude-module=test --exclude-module=unittest --exclude-module=setuptools --exclude-module=pydoc
-ren main.exe InvoiceConvert.exe
+ren dist\main.exe dist\InvoiceConvert.exe
